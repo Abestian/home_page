6481ad99a7c8742a7a9fb14e851533e1a7def377
Sebastian Kliś <klis.seb@gmail.com>
2024-03-31 13:12:56 +0200

    Created settings corresponding to saved tabs link alignment and added comments for better code readability.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let backgroundImg
 let settBtn
 let settPnl
 let settHead
-let languageSettings
+let settings
 let langPol
 let langEng
 let langDe
@@ -14,12 +14,19 @@ let savedTabsBtn
 let savedTabsPnl
 let savedTabsHead
 let chevronBtn
+let savedLink
+let alignTitle
+let alignmentLeft
+let alignmentCenter
+let alignmentRight
 
 let currentLanguage = 'en'
+let currentAlignment = 'center'
 
 const main = () => {
 	prepareDOMElements()
 	prepareDOMEvents()
+	alignCenter()
 }
 
 // function that downloads elements from html file and sets them to previously created variables
@@ -30,7 +37,7 @@ const prepareDOMElements = () => {
 	settBtn = document.querySelector('.settings-button')
 	settPnl = document.querySelector('.settings-panel')
 	settHead = document.querySelector('.settings-header')
-	languageSettings = document.querySelector('.language-settings')
+	settings = document.querySelector('.settings')
 	langPol = document.querySelector('.lang-polish')
 	langEng = document.querySelector('.lang-english')
 	langDe = document.querySelector('.lang-deutsch')
@@ -39,11 +46,16 @@ const prepareDOMElements = () => {
 	savedTabsPnl = document.querySelector('.savedtabs-panel')
 	savedTabsHead = document.querySelector('.savedtabs-header')
 	chevronBtn = document.querySelector('.fa-chevron-up')
+	savedLink = document.querySelectorAll('.saved-link')
+	alignTitle = document.querySelector('.link-align')
+	alignmentLeft = document.querySelector('.align-left')
+	alignmentCenter = document.querySelector('.align-center')
+	alignmentRight = document.querySelector('.align-right')
 }
 // function that adds event listeners
 const prepareDOMEvents = () => {
 	settBtn.addEventListener('click', showSettingsPanel)
-	languageSettings.addEventListener('click', checkClick)
+	settings.addEventListener('click', checkClickSettings)
 	savedTabsBtn.addEventListener('click', showSavedTabsPanel)
 }
 
@@ -92,8 +104,8 @@ const showCurrentDay = language => {
 	dayText.textContent = text
 	dayInfo.textContent = today
 }
-
-const checkClick = e => {
+// function that checks what option is clicked in settings panel
+const checkClickSettings = e => {
 	if (e.target.matches('.lang-polish')) {
 		setPolishLanguage()
 		showCurrentDay(currentLanguage)
@@ -103,15 +115,21 @@ const checkClick = e => {
 	} else if (e.target.matches('.lang-deutsch')) {
 		setDeutschLanguage()
 		showCurrentDay(currentLanguage)
+	} else if (e.target.matches('.align-left')) {
+		alignLeft()
+	} else if (e.target.matches('.align-center')) {
+		alignCenter()
+	} else if (e.target.matches('.align-right')) {
+		alignRight()
 	}
 }
 
 // three next functions account for setting texts and panels in a chosen language
 const setPolishLanguage = () => {
 	currentLanguage = 'pl'
-	langPol.classList.add('active-language')
-	langDe.classList.remove('active-language')
-	langEng.classList.remove('active-language')
+	langPol.classList.add('active-setting')
+	langDe.classList.remove('active-setting')
+	langEng.classList.remove('active-setting')
 
 	settHead.textContent = 'Ustawienia'
 	langTitle.textContent = 'Język:'
@@ -119,13 +137,18 @@ const setPolishLanguage = () => {
 	langPol.textContent = 'Polski'
 	langDe.textContent = 'Niemiecki'
 
+	alignTitle.textContent = 'Wyrównaj zapisane karty do: '
+	alignmentLeft.textContent = 'Lewej'
+	alignmentCenter.textContent = 'Środka'
+	alignmentRight.textContent = 'Prawej'
+
 	savedTabsHead.textContent = 'Zapisane Karty'
 }
 const setEnglishLanguage = () => {
 	currentLanguage = 'en'
-	langEng.classList.add('active-language')
-	langDe.classList.remove('active-language')
-	langPol.classList.remove('active-language')
+	langEng.classList.add('active-setting')
+	langDe.classList.remove('active-setting')
+	langPol.classList.remove('active-setting')
 
 	settHead.textContent = 'Settings'
 	langTitle.textContent = 'Language:'
@@ -133,14 +156,19 @@ const setEnglishLanguage = () => {
 	langPol.textContent = 'Polish'
 	langDe.textContent = 'German'
 
+	alignTitle.textContent = 'Align saved tabs: '
+	alignmentLeft.textContent = 'Left'
+	alignmentCenter.textContent = 'Center'
+	alignmentRight.textContent = 'Right'
+
 	savedTabsHead.textContent = 'Saved Tabs'
 }
 
 const setDeutschLanguage = () => {
 	currentLanguage = 'de'
-	langDe.classList.add('active-language')
-	langPol.classList.remove('active-language')
-	langEng.classList.remove('active-language')
+	langDe.classList.add('active-setting')
+	langPol.classList.remove('active-setting')
+	langEng.classList.remove('active-setting')
 
 	settHead.textContent = 'Einstellungen'
 	langTitle.textContent = 'Sprache:'
@@ -148,19 +176,57 @@ const setDeutschLanguage = () => {
 	langPol.textContent = 'Polnisch'
 	langDe.textContent = 'Deutsch'
 
+	alignTitle.textContent = 'Gespeicherte Ausrichtung der Tabs: '
+	alignmentLeft.textContent = 'Links'
+	alignmentCenter.textContent = 'Zentrum'
+	alignmentRight.textContent = 'Rechts'
+
 	savedTabsHead.textContent = 'Gespeicherte Tabs'
 }
 
-// function that toggles setting panel (also make the gear turn)
+// function that toggles setting panel
 const showSettingsPanel = () => {
 	settPnl.classList.toggle('show')
 	settBtn.classList.toggle('gear-turn')
 }
 
+// function that toggles saved tabs panel
 const showSavedTabsPanel = () => {
 	savedTabsPnl.classList.toggle('active-savedtabs')
 	savedTabsBtn.classList.toggle('active-savedtabsbtn')
 	chevronBtn.classList.toggle('chevron-rotate')
+}
+
+// three next functions align links in saved tabs to left/center/right
+const alignLeft = () => {
+	savedLink.forEach(link => {
+		link.classList.add('align-left')
+		link.classList.remove('align-center')
+		link.classList.remove('align-right')
+	})
+	alignmentLeft.classList.add('active-alignment')
+	alignmentCenter.classList.remove('active-alignment')
+	alignmentRight.classList.remove('active-alignment')
+}
+const alignCenter = () => {
+	savedLink.forEach(link => {
+		link.classList.add('align-center')
+		link.classList.remove('align-left')
+		link.classList.remove('align-right')
+	})
+	alignmentCenter.classList.add('active-alignment')
+	alignmentLeft.classList.remove('active-alignment')
+	alignmentRight.classList.remove('active-alignment')
+}
+const alignRight = () => {
+	savedLink.forEach(link => {
+		link.classList.add('align-right')
+		link.classList.remove('align-center')
+		link.classList.remove('align-left')
+	})
+	alignmentRight.classList.add('active-alignment')
+	alignmentLeft.classList.remove('active-alignment')
+	alignmentCenter.classList.remove('active-alignment')
 }
 
 // event listeners that run functions as soon as the web page is opened
